5bf0391cdebeb75b2092e75d1799517365398b40
Ime Prezime <moj_email@gmail.com>
2019-07-30 19:21:06 +0200

    Change by LalujaB

--- single-post.php
+++ single-post.php
@@ -4,7 +4,9 @@ include ('include/header.php');
 
 ?>
 
-    <link rel="stylesheet" href="vivifyacademy-basic-blog-boilerplate/styles/styles.css" type="text/css">
+
+    <link rel="stylesheet" href="styles/blog.css" type="text/css">
+    <link rel="stylesheet" href="styles/styles.css" type="text/css">
 
     <main role="main" class="container">
 
@@ -13,7 +15,7 @@ include ('include/header.php');
             <div class="col-sm-8 blog-main">
 
                 <div class="blog-post">
-                    <h2 class="blog-post-title">Sample blog post</h2>
+                    <a href "#"> <h2 class="blog-post-title">Sample blog post</h2></a>
                     <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
 
                     <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
@@ -23,7 +25,7 @@ include ('include/header.php');
                         <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
                     </blockquote>
                     <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                    <h2>Heading</h2>
+                    <a href "#"> <h2>Heading</h2></a>
                     <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
                     <h3>Sub-heading</h3>
                     <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
@@ -46,7 +48,7 @@ include ('include/header.php');
                 </div><!-- /.blog-post -->
 
                 <div class="blog-post">
-                    <h2 class="blog-post-title">Another blog post</h2>
+                    <a href "#"><h2 class="blog-post-title">Another blog post</h2></a>
                     <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
 
                     <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
@@ -58,7 +60,7 @@ include ('include/header.php');
                 </div><!-- /.blog-post -->
 
                 <div class="blog-post">
-                    <h2 class="blog-post-title">New feature</h2>
+                    <a href "#"><h2 class="blog-post-title">New feature</h2></a>
                     <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
 
                     <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
