d6b3fc30c39a20eb2f1e37d32b34e6df803c4011
Ime Prezime <moj_email@gmail.com>
2019-08-01 19:39:04 +0200

    Change by LalujaB

--- posts.php
+++ posts.php
@@ -39,10 +39,7 @@ $posts = database($sql, $connection, 'fetchAll');
 
 
 
-                <nav class="blog-pagination">
-                    <a class="btn btn-outline-primary" href="#">Older</a>
-                    <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
-                </nav>
+
 
             </div><!-- /.blog-main -->
 
